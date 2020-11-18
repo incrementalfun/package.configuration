f8d7f6d7c7400836967c3863bde4535823bce0fd
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-11-18 09:40:36 +0100

    Added cloudwatch sink

--- src/Incremental.Common.Configuration/ConfigurationExtension.cs
+++ src/Incremental.Common.Configuration/ConfigurationExtension.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using Amazon.CloudWatchLogs;
+using Amazon.Runtime;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using Serilog.Exceptions;
+using Serilog.Sinks.AwsCloudWatch;
+using Serilog.Sinks.AwsCloudWatch.LogStreamNameProvider;
 
 namespace Incremental.Common.Configuration
 {
@@ -19,27 +23,41 @@ namespace Incremental.Common.Configuration
         {
             return new ConfigurationBuilder()
                 .SetBasePath(directory)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true)
+                .AddJsonFile($"appsettings.Local.json", true)
                 .AddEnvironmentVariables()
                 .Build();
         }
-        
+
         /// <summary>
         /// Creates a logger using Serilog.
         /// </summary>
         /// <param name="configuration"></param>
+        /// <param name="service"></param>
         /// <returns></returns>
-        public static ILogger LoadLogger(IConfiguration configuration)
+        public static ILogger LoadLogger(IConfiguration configuration, string service)
         {
+            var environment = $"{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}";
+            
+            var cloudWatchSink = new CloudWatchSinkOptions
+            {
+                LogGroupName = "incremental",
+                LogStreamNameProvider = new ConfigurableLogStreamNameProvider($"{service}#{environment}", false, false),
+                CreateLogGroup = true,
+                LogGroupRetentionPolicy = LogGroupRetentionPolicy.OneWeek
+            };
+            
+            var client = new AmazonCloudWatchLogsClient(new BasicAWSCredentials(configuration["AWS_ACCESS_KEY"], configuration["AWS_SECRET_KEY"]));
+
             var loggerConf = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails()
-                .WriteTo.Console();
-            
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                .WriteTo.AmazonCloudWatch(cloudWatchSink, client);
+
+            if (environment == "Development")
             {
+                loggerConf.WriteTo.Console();
                 loggerConf.MinimumLevel.Debug();
             }
             else
