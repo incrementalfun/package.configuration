cf7f484bc0c9f10b353a4fd4b693e90813bdb6a7
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-05-09 16:54:11 +0200

    feat: disable some log sinks when in testing env

--- src/Incremental.Common.Configuration/CommonLoggingExtensions.cs
+++ src/Incremental.Common.Configuration/CommonLoggingExtensions.cs
@@ -32,28 +32,28 @@ namespace Incremental.Common.Configuration
         {
             configuration ??= CommonConfigurationExtensions.BuildConfiguration(Directory.GetCurrentDirectory());
 
-            var environment = $"{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}";
-
-            var cloudWatchSink = new CloudWatchSinkOptions
-            {
-                LogGroupName = $"{configuration["LOG_GROUP_NAME"]}#{environment}",
-                LogStreamNameProvider = new ConfigurableLogStreamNameProvider($"{Assembly.GetCallingAssembly().GetName().Name}", false, false),
-                CreateLogGroup = false,
-                TextFormatter = new CompactJsonFormatter(),
-                MinimumLogEventLevel = environment == "Development" ? LogEventLevel.Debug : LogEventLevel.Information
-            };
-
-            var client = new AmazonCloudWatchLogsClient(new BasicAWSCredentials(configuration["AWS_ACCESS_KEY"], configuration["AWS_SECRET_KEY"]),
-                RegionEndpoint.EUWest1);
+            var environment = $"{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown"}";
 
             var loggerConf = new LoggerConfiguration()
                 .Enrich.FromLogContext()
                 .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder()
                     .WithDefaultDestructurers()
-                    .WithDestructurers(new[] {new DbUpdateExceptionDestructurer()}))
-                .WriteTo.AmazonCloudWatch(cloudWatchSink, client);
+                    .WithDestructurers(new[] {new DbUpdateExceptionDestructurer()}));
 
-            if (environment == "Development")
+            if (environment is "Testing")
+            {
+                loggerConf.WriteTo.AmazonCloudWatch(new CloudWatchSinkOptions
+                {
+                    LogGroupName = $"{configuration["LOG_GROUP_NAME"]}#{environment}",
+                    LogStreamNameProvider = new ConfigurableLogStreamNameProvider($"{Assembly.GetCallingAssembly().GetName().Name}", false, false),
+                    CreateLogGroup = false,
+                    TextFormatter = new CompactJsonFormatter(),
+                    MinimumLogEventLevel = environment == "Development" ? LogEventLevel.Debug : LogEventLevel.Information
+                }, new AmazonCloudWatchLogsClient(new BasicAWSCredentials(configuration["AWS_ACCESS_KEY"], configuration["AWS_SECRET_KEY"]),
+                    RegionEndpoint.EUWest1));
+            }
+            
+            if (environment is "Development" or "Testing")
             {
                 loggerConf.WriteTo.Console();
                 loggerConf.MinimumLevel.Debug();
