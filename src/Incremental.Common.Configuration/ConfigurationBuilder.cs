c312365f1ee407369ab890b229019ff7becac76d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-08-17 13:47:01 +0300

    feat: move log support to other package

--- src/Incremental.Common.Configuration/ConfigurationBuilder.cs
+++ src/Incremental.Common.Configuration/ConfigurationBuilder.cs
@@ -6,24 +6,8 @@ namespace Incremental.Common.Configuration
     /// <summary>
     ///     Common set of configuration settings.
     /// </summary>
-    public static class CommonConfigurationExtensions
+    public static class ConfigurationBuilder
     {
-        /// <summary>
-        ///     Builds an IConfiguration.
-        /// </summary>
-        /// <param name="directory">Directory of the project.</param>
-        /// <returns>An <see cref="IConfiguration" />.</returns>
-        internal static IConfiguration BuildConfiguration(string directory)
-        {
-            return new ConfigurationBuilder()
-                .SetBasePath(directory)
-                .AddJsonFile("appsettings.json", false, true)
-                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Unknown"}.json", true)
-                .AddJsonFile("appsettings.Local.json", true)
-                .AddEnvironmentVariables()
-                .Build();
-        }
-
         /// <summary>
         ///     Adds common configuration sources to the <see cref="IConfigurationBuilder" />.
         /// </summary>
